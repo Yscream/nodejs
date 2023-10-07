3d1e0accd4c603934b2a8ebef012cdb6cbb2d9e8
Yehor Kopyl <kopyl.egor1@gmail.com>
2023-10-07 15:44:20 +0300

    http

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const http = require("http");
+const EventEmitter = require('events');
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080
+const emitter = new EventEmitter;
+
+class Router {
+    constructor() {
+        this.endpoints = {}
+    }
+
+    request(method = "GET", path, handler) {
+        if (!this.endpoints[path]) {
+             this.endpoints[path] = {}
+        }
+        const endpoint = this.endpoints[path]
+
+        if (endpoint[method]) {
+            throw new Error(`${method} by address ${path} already exists`)
+        }
+
+        endpoint[method] = handler
+    }
+}
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'application/json'
-    })
-    if (req.url === '/users') {
-        return res.end(JSON.stringify([{
-            id:1, name: "Yehor"
-        }]))
-    }
-    if (req.url === '/posts') {
-        res.end('POSTS')
-    }
+   res.end(req.url);
 });
 
 server.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
